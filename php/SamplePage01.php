753d44b272505515c3c362e48d51c2d75d249c4c
reuel <reuel@LPT-308.mshome.net>
2019-12-18 19:07:29 +0800

    PHP Tutorial * Concatenation Operator Part 1 * Escape Sequence Part 1

--- php/SamplePage01.php
+++ php/SamplePage01.php
@@ -15,23 +15,48 @@
 </head>
 <body>
 
-	<div class="container">
+	<div class="fluid-container">
 		<div class="row">
-			<div class="col-lg-2"></div>
-			<div class="col-lg-8">
-				<h1 title="topic1">Headline 1</h1>
-				First Name : <input type="text" value="John" /> 
-				<br /> <br /> 
-				Last Name : <input type="text" value="Major" /> 
-				<br /> <br /> 
-				Gender : <input type="radio" name="gender" checked="checked" value="Male">Male 
-						 <input type="radio" name="gender" value="Female">Female 
-						 <br /> <br />
-				Skills : <input type="checkbox" name="skills" checked="checked" value="JavaScript" />JavaScript 
-				         <input type="checkbox" name="skills" checked="checked" value="jQuery" />jQuery 
-				         <input type="checkbox" name="skills" value="C#" />C# 
-				         <br /> <br />
-				Country: <select>
+			<div class="col-sm-1"></div>
+			<div class="col-sm-10">
+    			<div class="row">
+    				<div class="col-sm-12">
+    					<h1 title="topic1">Headline 1</h1>
+    				</div>
+				<div class="row">
+    				<div class="col-sm-12">
+    				<?php 
+						$uri="/SampleProject01/php/SamplePage02.php";
+                    ?>
+    				<a href=<?php echo $uri  ?>>SamplePage02</a>
+    				</div>
+    			</div>
+    			</div>
+    			<div class="row">
+					<div class="col-sm-5">
+						First Name : <input type="text" value="John" /> 
+					</div>
+					<div class="col-sm-7">
+						Last Name : <input type="text" value="Major" /> 
+					</div>    			
+    			</div>
+    			
+    			<div class="row">
+        			<div class="col-sm-5">
+        			
+    						Gender : <input type="radio" name="gender" checked="checked" value="Male">Male 
+    						 <input type="radio" name="gender" value="Female">Female 
+        			</div>
+    				<div class="col-sm-7">
+        			
+    						Skills : <input type="checkbox" name="skills" checked="checked" value="JavaScript" />JavaScript 
+    				         <input type="checkbox" name="skills" checked="checked" value="jQuery" />jQuery 
+    				         <input type="checkbox" name="skills" value="C#" />C# 
+        			</div>
+    			</div>
+    			<div class="row">
+    				<div class="col-sm-12">
+    					Country: <select>
 							<?php 
 							$Country="Philippines";
 							$Countries=array(Japan, Singapore);
@@ -39,25 +64,44 @@
         					<option selected="selected" value="USA">USA</option>
         					<option value="India">India</option>
         					<option value="UK">UK</option>
-        					<option value=<?php echo $Country?>><?php echo $Country?></option>
-        					<option value=<?php echo $Countries[0]?>><?php echo $Countries[0]?></option>
-        					<option value=<?php echo $Countries[1]?>><?php echo $Countries[1]?></option>
+        					<option value=<?php echo $Country ?>><?php echo $Country ?></option>
+        					<option value=<?php echo $Countries[0] ?>><?php echo $Countries[0] ?></option>
+        					<option value=<?php echo $Countries[1] ?>><?php echo $Countries[1] ?></option>
 						</select> 
-						<br /> <br /> 
-				Summary : <br />
-						<textarea>
+  					</div>
+    			</div>
+    			<div class="row">
+    				<div class="col-sm-12">
+    					Summary : <br />
+						<textarea cols="50" rows="5">
+        					
+        					<?php 
+        					   // Concatenation symbol "."
+        					   echo 'Conca-- ' .$Country. ' --tenate'."\n";  
+        					   
+        					   // Double qoutes call variable value
+        					   echo "Hello $Country \n"; 
+        					   
+        					   // Single qoutes treats the variable as text
+        					   echo 'Hello $Country '."\n"; 
+        					?>
         					I am a Senior Dot Net Developer with 10 years experience
-    					</textarea>
-						<br /> <br /> 
-				<input id="Submit1" type="submit" value="Submit input" />
-				<input id="Submit2" type="submit" value="Submit :input" />
+	    					</textarea>
+    				</div>
+    			</div>
+    			<div class="row">
+					<div class="col-sm-5">
+						<input id="Submit1" type="submit" value="Submit input" />
+					</div>
+					<div class="col-sm-7">
+						<input id="Submit2" type="submit" value="Submit :input" />
+					</div> 
+    			</div>
+
+    			
 			</div>
-			<div class="col-lg-2"></div>
+			<div class="col-sm-1"></div>
 		</div>
 	</div>
-
-
-
-
 </body>
 </html>
