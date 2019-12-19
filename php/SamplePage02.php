c11f4533021591263b8568bcc7b62365b2395ec3
reuel <reuel@LPT-308.mshome.net>
2019-12-19 18:45:09 +0800

    PHP Tutorial URL: https://www.php.net/manual/en/language.types.boolean.php
    Title
    * Types - Boolean

--- php/SamplePage02.php
+++ php/SamplePage02.php
@@ -1,6 +1,8 @@
 <?php
 
 // Convertiong Other TYPES ==> BOOLEAN
+// var_dump() function is used to display 
+// structured information (type and value) 
 // ====================================
 $var_name1=678;
 $var_name2="a678";
@@ -8,14 +10,16 @@ $var_name3="678";
 $var_name4="W3resource.com";
 $var_name5=698.99;
 $var_name6=+125689.66;
-echo var_dump((bool) $var_name1)."<br>";
-echo var_dump((bool) $var_name2)."<br>";
-echo var_dump((bool) $var_name3)."<br>";
-echo var_dump((bool) $var_name4)."<br>";
-echo var_dump((bool) $var_name5)."<br>";
-echo var_dump((bool) $var_name6)."<br><br>";
+echo var_dump($var_name1)."<br>";
+echo var_dump($var_name2)."<br>";
+echo var_dump($var_name3)."<br>";
+echo var_dump($var_name4)."<br>";
+echo var_dump($var_name5)."<br>";
+echo var_dump($var_name6)."<br><br>";
 
-
+// Convertiong Other TYPES ==> BOOLEAN
+// Force to BOOLEAN TYPE
+// ====================================
 echo var_dump((bool) "")."<br>";        // bool(false)
 echo var_dump((bool) 1)."<br>";         // bool(true)
 echo var_dump((bool) -2)."<br>";        // bool(true)
